12b22d365190bd59534ab1c02d931a34d676d413
dongzhanxie <sam199948@163.com>
2021-09-26 15:03:52 +0100

    Add Project Document

--- homepage/models.py
+++ homepage/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+
+# User Model
+class User(models.Model):
+    name = models.CharField(max_length=128, unique=True)
+    # date = models.DateField()
+
+    def __str__(self):
+        return self.name
+
+
+# Bike System Model
+class MySystem(models.Model):
+    location = models.IntegerField(default=0)
+    url = models.URLField()
+
+    def __str__(self):
+        return self.location
+
+
--- homepage/views.py
+++ homepage/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.contrib import staticfiles
+from homepage import userscript
 
 
 def index(request):
@@ -9,4 +10,5 @@ def index(request):
 
 
 def rent(request):
+    userscript.add_location(221221)
     return HttpResponse("Let's rent now   <a href='/homepage/'>Index</a>")
